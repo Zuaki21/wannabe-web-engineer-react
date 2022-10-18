0a807b06e00f366b34e69132c6f2b63e6a53d990
Zuaki <67538904+Zuaki21@users.noreply.github.com>
2022-10-19 01:14:16 +0900

    Todoリスト途中

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import Counter from "./components/Counter";
+import ItemList from "./components/ItemList";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <ItemList />
       <Counter />
     </div>
   );
 }
 
 export default App;
+
+// import Counter from "./components/Counter";
+// import "./App.css";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Counter />
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/components/Item.jsx
@@ -0,0 +1,10 @@
+const Item = (props) => {
+  return (
+    <div style={{ color: props.price >= 500 ? "red" : "black" }}>
+      <div>名前：{props.name}</div>
+      <div>価格：{props.price}</div>
+      {props.price >= 10000 && <div>高額商品</div>}
+    </div>
+  );
+};
+export default Item;
